cc56ccc161fa54007a2d171a090f954eccb2a144
Romaric Zounlome <rzounlom@gmail.com>
2024-05-15 11:51:34 -0400

    initial commit; install dependencies

---
+++ src/data/movies.js
@@ -0,0 +1,14 @@
+export const defaultMovies = [
+  {
+    id: 1,
+    title: "Dune",
+    description:
+      "Paul Atreides, a brilliant and gifted young man born into a great destiny beyond his understanding, must travel to the most dangerous planet in the universe to ensure the future of his family and his people. As malevolent forces explode into conflict over the planet's exclusive supply of the most precious resource in existence, only those who can conquer their own fear will survive.",
+    year: 2021,
+    genre: "Science Fiction",
+    ratings: [],
+    imgUrl:
+      "https://m.media-amazon.com/images/M/MV5BMDQ0NjgyN2YtNWViNS00YjA3LTkxNDktYzFkZTExZGMxZDkxXkEyXkFqcGdeQXVyODE5NzE3OTE@._V1_FMjpg_UX1000_.jpg",
+    trailorUrl: "https://www.youtube.com/watch?v=n9xhJrPXop4",
+  },
+];
