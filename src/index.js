02044a3890c2e22991a3a3ad8dd8345908bc5add
Romaric Zounlome <rzounlom@gmail.com>
2024-05-21 07:52:00 -0400

    revert react version from 18 to 17

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ import "./index.css";
 
 import App from "./App";
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
